4e3adb084a0ec2c34b5fcf3866fee541aa4283a9
ribhu <ribhu02@gmail.com>
2021-08-28 23:41:39 +0530

    edit react editor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is HomePage
+      </Route>
+
+      <Route exact path="/starred">
+        This is Starred
+      </Route>
+
+      <Route>This is 404 page</Route>
+    </Switch>
+  );
 }
 
 export default App;
