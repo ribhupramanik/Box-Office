4ebc1439f37fc4ce33db048895ac5a927826d20a
ribhu <ribhu02@gmail.com>
2021-08-30 15:13:00 +0530

    Api Integration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPAgeLayout from '../components/MainPAgeLayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
-  return <MainPAgeLayout>This is home</MainPAgeLayout>;
+  const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
+
+  const onSearch = () => {
+    apiGet('/search/shows?q=$(input)').then(result => {
+      setResults(result);
+    });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+  const renderResult = () => {
+    if (results && results.length === 0) {
+      return <div>No Result</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <MainPAgeLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+      {renderResult()}
+    </MainPAgeLayout>
+  );
 };
 
 export default Home;
